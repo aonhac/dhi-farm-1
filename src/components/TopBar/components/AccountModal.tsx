f4c700eee6e032264aa8f3dbdc10ccc185177fff
aonhac <105034482+aonhac@users.noreply.github.com>
2022-09-15 19:07:35 +0700

    Update AccountModal.tsx

--- src/components/TopBar/components/AccountModal.tsx
+++ src/components/TopBar/components/AccountModal.tsx
@@ -39,7 +39,7 @@ const AccountModal: React.FC<ModalProps> = ({ onDismiss }) => {
             </CardIcon>
             <StyledBalance>
               <Value value={getBalanceNumber(sushiBalance)} />
-              <Label text="DHI Balance" />
+              <Label text="ALK Balance" />
             </StyledBalance>
           </StyledBalanceWrapper>
         </div>
